0903dea289102445e0a5677d5c200a64f2024003
yann.lesage <yannlesag@gmail.com>
2021-10-25 08:42:27 +0200

    chores: add local files

--- resources/javascript/binding.js
+++ resources/javascript/binding.js
@@ -1,69 +1,107 @@
 class MediumEditorSt {
-  static singleton
+  static instancesMap = new WeakMap()
+  static extensions = {}
 
-  static init() {
-    MediumEditorSt.getInstance()
+  static initNotLoadedEditor() {
+    let editors = Array.from(document.querySelectorAll(`.meditor-editable[data-loaded="false"]`))
+    editors.forEach(html => {
+      new MediumEditorSt(html)
+    })
   }
 
-  static getInstance() {
-    if (!this.singleton) {
-      this.singleton = new MediumEditorSt()
+  static getInstance(querySelector) {
+    return this.instancesMap.get(document.querySelector(querySelector))
+  }
+
+  static convertor() {
+    if (!this.showdownConvertor) {
+      this.showdownConvertor = new showdown.Converter({extensions: ['xssfilter']})
     }
-    return this.singleton
+    return this.showdownConvertor
   }
 
-  elementNodeListOf = this.formatNewEditors()
+  static save(selector) {
+    document.body.style.cursor = "progress"
+    return MediumEditorSt.getInstance(selector).getValue()
+  }
 
-  constructor() {
+  static saveCompleted() {
+    document.body.style.cursor = "default"
+  }
+
+  static saveFailed() {
+    document.body.style.cursor = "default"
+  }
+
+  constructor(editor) {
     this.delayMap = new Map()
-    let editors = this.elementNodeListOf
-    this.mediumEditors = new MediumEditor(editors)
+    this.editor = editor
+    let options = {
+      relativeContainer: editor.parentElement,
+      extensions: {}
+    }
+    let extensions = JSON.parse(this.editor.dataset["extensions"])
+    if (extensions && extensions.length) {
+      extensions.forEach(extName => {
+        options.extensions[extName.toLowerCase()] = Function(`return new ${extName}()`)()
+      })
+    }
+    let buttons = JSON.parse(this.editor.dataset["buttons"])
+    if (buttons && buttons.length) {
+      options.toolbar = {
+        buttons: []
+      }
+      buttons.forEach(extName => {
+        options.toolbar.buttons.push(extName.toLowerCase())
+      })
+    }
+    console.log(options)
+    this.editor.innerHTML = this.constructor.convertor().makeHtml(this.editor.innerHTML)
+    this.mediumEditors = new MediumEditor([this.editor], options)
 
-    this.mediumEditors.subscribe("editableInput", (event, editable)=> {
-      this.delay( "onInputChange", 1000,()=> {
+    this.mediumEditors.subscribe("editableInput", (event, editable) => {
+      this.delay("onInputChange", 1000, () => {
         this.onInputChange(event, editable)
       })
     })
-  }
 
-  formatNewEditors() {
-    let editors = document.querySelectorAll(".meditor-editable.not-loaded")
-    editors.forEach(html => {
-      html.innerHTML = this.convertor().makeHtml(html.innerHTML)
-    })
-    return editors
+    MediumEditorSt.instancesMap.set(editor, this)
   }
 
   onInputChange(event, editable) {
     console.log(editable, event)
   }
 
-  getValueForEditor(htmlRootEditor) {
-    return this.convertor().makeMarkdown(htmlRootEditor.innerHTML)
-  }
-
-  addNotLoadedEditors() {
-    let editors = document.querySelectorAll(".meditor-editable.not-loaded")
-    this.mediumEditors.addElements(editors)
-  }
-
-  removeEditors(elements) {
-    this.mediumEditors.removeElements(elements)
+  getValue() {
+    return this.constructor.convertor().makeMarkdown(this.editor.innerHTML)
+      .replace(/<(\/)?br>/g, "")
+      .replace(/\(<([.\w\/]*)>\)/g, "($1)")
   }
 
   delay(key, duration, fct) {
-    if(this.delayMap.has(key)) {
+    if (this.delayMap.has(key)) {
       clearTimeout(this.delayMap.get(key))
     }
-    this.delayMap.set(key, setTimeout(fct, duration))
-  }
-
-  convertor() {
-    if (!this.showdownConvertor) {
-      this.showdownConvertor = new showdown.Converter({extensions: ['xssfilter']})
-    }
-    return this.showdownConvertor
+    this.delayMap.set(key, setTimeout(() => {
+      this.delayMap.delete(key)
+      fct()
+    }, duration))
   }
 }
+
+window.addEventListener("load", MediumEditorSt.initNotLoadedEditor)
+
+let SaveButton = MediumEditor.extensions.button.extend({
+  name: 'savebutton',
+  contentDefault: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M15.004 3h2.996v5h-2.996v-5zm8.996 1v20h-24v-24h20l4 4zm-19 5h14v-7h-14v7zm16 4h-18v9h18v-9zm-2 2h-14v1h14v-1zm0 2h-14v1h14v-1zm0 2h-14v1h14v-1z"/></svg>', // default innerHTML of the button
+  aria: 'save',
+  action: 'save',
+  init: function () {
+    MediumEditor.extensions.button.prototype.init.call(this)
+  },
+  handleClick: function (event) {
+    Function('"use strict";return ('+this.base.elements[0].dataset["saveCallback"]+ ')')(this)
+  }
+})
 
 window.addEventListener("load", MediumEditorSt.init)
